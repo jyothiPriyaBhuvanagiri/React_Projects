c2364e6b4c13ea400181c0da3fa145b7d14916bf
Jyothipriya Bhuvanagiri <108977425+jyothiPriyaBhuvanagiri@users.noreply.github.com>
2024-08-14 20:10:41 +0200

    intial commit

---
+++ React_projects/to_do_app/src/App.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import './App.css';
+
+
+function App() {
+  const [taskList, setTaskList] = useState([]);
+
+  return (
+      <div className="px-12">
+        <div className="flex flex-col items-center justify-center py-8 gap-4">
+          <h1 className="text-xl font-semibold">To-Do-Board</h1>
+          <Input taskList={taskList} setTaskList={setTaskList} />
+        </div>
+        <div className="flex flex-col gap-4 sm:grid sm:grid-cols-3 px-4 sm:px-8 md:px-10">
+          {taskList.map((task, index) => (
+              <Board
+                  key={index}
+                  index={index}
+                  task ={task}
+                  taskList={taskList}
+                  setTaskList={setTaskList}
+              />
+          ))}
+        </div>
+      </div>
+  );
+}
+
+export default App;
