fac8fbff9f37b422b9603d5e165dab8138fdc5c8
Jyothipriya Bhuvanagiri <108977425+jyothiPriyaBhuvanagiri@users.noreply.github.com>
2024-08-17 10:19:19 +0200

    added function

--- React_projects/drag_and_drop/src/App.js
+++ React_projects/drag_and_drop/src/App.js
@@ -2,15 +2,22 @@ import { HTML5Backend } from 'react-dnd-html5-backend'
 import { DndProvider } from 'react-dnd'
 import Bin from "./components/Bin";
 import Note from "./components/note";
-import {useEffect} from "react";
+import {useState, useEffect} from "react";
 
 function App() {
-    const [notes, setNotes] = React.useState([]);
-    const[binned, setBinned] = React.useState([]);
+    const [notes, setNotes] = useState([]);
+    const[binned, setBinned] = useState([]);
 
-    useEffect( () =>{
+    useEffect(() => {
+       const initialNotes = ["Note 1", "Note 2", "Note 3"]
+        localStorage.setItem("noteList", JSON.stringify(initialNotes))
 
-        }, [])
+       let array = localStorage.getItem("noteList");
+       setNotes(JSON.parse(array));
+
+    }, []);
+console.log(notes)
+
   return (
     <DndProvider backend={HTML5Backend}>
       <h1 className="text-center text-3xl font-semibold mt-4 py-2">Drag And Drop Application</h1>
